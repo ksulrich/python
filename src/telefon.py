2744e42195aa35fdefafe22e8b549b0b7070c9b6
Klaus Ulrich <klaus.ulrich@web.de>
2013-10-13 13:38:01 +0200

    remove print outs, comment code

--- src/telefon.py
+++ src/telefon.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # $Id$
 #
-# Usage: ./telefon.py < $HOME/Wissen/telefon.txt <pattern>
+# Usage: ./telefon.py $HOME/Wissen/telefon.txt <pattern>
 
 import re
 import sys
 
+# pats holds the list of regex patterns to search for
 pats = list()
+
+# linePat is the delimiter between entries
 linePat = re.compile("^---")
+
+# we need at least the file and one pattern
 if len(sys.argv) <= 2:
     print """
-Usage: telefon.py <telefon> <pattern_1> <pattern_2> ...  <pattern_n>
+    Usage: telefon.py <telefon> <pattern_1> <pattern_2> ...  <pattern_n>
        where <pattern_i> is a regular expression like "ul.*"
        The patterns are ANDed, so you can search for that:
        ./telefon.py telefon.txt klaus "ul.*"
@@ -19,41 +24,44 @@ Usage: telefon.py <telefon> <pattern_1> <pattern_2> ...  <pattern_n>
         """
     sys.exit(1)
 
+# The file is the first argument
 file = sys.argv[1]
 for i in sys.argv[2:]:
+    # the next arguments are regex patterns, we ignore case
     pats.append(re.compile(i, re.IGNORECASE))
 
-def main():
+# Do the job
+def run():
+    # Reset text to none
     text = None
     for line in open(file, "r"):
-        #print line,
+        # do we have a line pattern?
         if linePat.search(line):
-            #print "--- found"
             # first or next entry found
             if not text:
-                #print "===reset text 1"
                 # first entry
                 text = ""
             else:
-                # entry ended -> search for pattern
-                #print "TEXT:", text
+                # entry ended -> search for pattern, text contains the data to search for
+                # set found marker
                 found = True
                 for p in pats:
                     match = p.search(text)
                     if (match):
+                        # yes, we have a match
                         found &= True
                     else:
+                        # no match, reset found marker to False
                         found = False
                 if found:
+                    # all the pattern did match with the text, print it
                     print "%s" %(79*"-")
                     print "%s" % text,
-                # new entry found -> reset text
-                #print "===reset text 2"
+                # we finished the entry and need to reset text
                 text = ""
         elif text is not None:
-            # we need to collect data
-            #print "===add line"
+            # we are not at a line delimiter, so we need to collect data in var text
             text += line
 
 if __name__ == "__main__":
-    main()
+    run()
