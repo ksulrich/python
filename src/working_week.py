8d41019909ba02cfc18ba7f2192749b1a769658c
Klaus Ulrich <klaus.ulrich@web.de>
2013-10-13 14:09:28 +0200

    reviewed and tested

--- src/working_week.py
+++ src/working_week.py
@@ -6,20 +6,24 @@
 import sys
 import datetime
 
-found = False
-week = 0
+def main():
+    found = False
+    week = 0
 
-for line in sys.stdin:
-    date, hours = line.split()
-    year, month, day = date.split('-')
-    #print "xxx:", year, month, day, "->", hours
-    day = datetime.date(int(year), int(month), int(day))
-    if (day.weekday() == 0):
-        if (found):
-            print "From %s to %s -> %1.1f hours" % (monday, lastday, float(week)/10)
-        found = True;
-        week = int(hours)
-        monday = day;
-    elif (found):
-        week += int(hours)
-        lastday = day
+    for line in sys.stdin:
+        date, hours = line.split()
+        year, month, day = date.split('-')
+        #print "xxx:", year, month, day, "->", hours
+        day = datetime.date(int(year), int(month), int(day))
+        if (day.weekday() == 0):
+            if (found):
+                print "From %s to %s -> %1.1f hours" % (monday, lastday, float(week) / 10)
+            found = True
+            week = int(hours)
+            monday = day
+        elif (found):
+            week += int(hours)
+            lastday = day
+
+if __name__ == "__main__":
+    main()
