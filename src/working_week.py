386862f97670c40a228e115d86559fabd9a055f8
Klaus Ulrich <klaus.ulrich@web.de>
2013-10-13 14:28:55 +0200

    comment code and fix it to run with working.py script

--- src/working_week.py
+++ src/working_week.py
@@ -7,8 +7,10 @@ import sys
 import datetime
 
 def main():
-    found = False
+    haveData = False
     week = 0
+    monday = None
+    lastday = None
 
     for line in sys.stdin:
         date, hours = line.split()
@@ -16,13 +18,21 @@ def main():
         #print "xxx:", year, month, day, "->", hours
         day = datetime.date(int(year), int(month), int(day))
         if (day.weekday() == 0):
-            if (found):
+            # we have a Monday
+            # do we have data for the last week?
+            if (haveData):
+                # yes, print it
+                # monday is the last monday we save, lastday is the last day we summed up the hours
                 print "From %s to %s -> %1.1f hours" % (monday, lastday, float(week) / 10)
-            found = True
+            # we have data, so start to sum up the weekly work hours
+            haveData = True
             week = int(hours)
+            # save this Monday in var monday for the next print out
             monday = day
-        elif (found):
+        elif (haveData):
+            # we started to sum up weekly work hours
             week += int(hours)
+            # and remember this day in var lastday used for print out
             lastday = day
 
 if __name__ == "__main__":
